e1024c7e3ac64492cdd1b14e9a03994cb377bdab
Ayberk Arıcı <ayberk816gg@gmail.com>
2024-01-19 14:03:50 +0300

    django downgrade

--- crawin/settings.py
+++ crawin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=k*hr@jyfwhlwy7)xwef4%%icxvn3_mcyn0&&=5+%*pkrq+$%(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
